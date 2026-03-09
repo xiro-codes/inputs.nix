9520c11aac4a42cb1f692d84d82cd4a1ec173aab
Travis Davis <me@tdavis.dev>
2026-03-08 21:06:11 -0500

    feat: Convert to flake-parts and add various NixOS and home-manager module inputs.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,60 @@
 {
-  description = "A very basic flake";
+  description = "Central dependecy flake for dotfiles.nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    stylix.url = "github:danth/stylix";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    gog-nix = {
+      url = "github:xiro-codes/gog.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@ { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      flake = {
+        inherit inputs;
+        nixosModules.default = {
+          imports = [
+            inputs.disko.nixosModules.disko
+            inputs.sops-nix.nixosModules.sops
+            inputs.home-manager.nixosModules.home-manager
+            inputs.nix-flatpak.nixosModules.nix-flatpak
+          ];
+        };
+        homeModules.default = {
+          imports = [
+            inputs.sops-nix.homeModules.sops
+            inputs.caelestia-shell.homeManagerModules.default
+            inputs.nixvim.homeModules.nixvim
+            inputs.stylix.homeModules.stylix
+          ];
+        };
+      };
+    };
 }
