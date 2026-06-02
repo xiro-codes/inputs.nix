2a6b8a453ef6be7a7940815cc03d8e8337a5548b
Travis Davis <me@tdavis.dev>
2026-06-02 11:06:39 -0500

    chore(flake): update nixpkgs and flake inputs
    Updated `flake.lock` to newer `nixpkgs` and other dependencies.
    `nixvim` input now resolves its own `nixpkgs` (unstable).

--- flake.nix
+++ flake.nix
@@ -45,10 +45,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixvim.url = "github:nix-community/nixvim";
 
     disko = {
       url = "github:nix-community/disko/latest";
